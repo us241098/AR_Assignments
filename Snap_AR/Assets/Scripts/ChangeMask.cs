0a2fa4807bbd0cf63ac56c0784193904151932f4
utsav shukla <us241077@gmail.com>
2020-03-29 06:38:08 +0530

    first

---
+++ Snap_AR/Assets/Scripts/ChangeMask.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMask : MonoBehaviour {
+    public GameObject glasses;
+    public GameObject foxMask;
+    public GameObject horseMask; 
+	
+    public void ChooseGlasses()
+    {
+        glasses.SetActive(true);
+        foxMask.SetActive(false);
+        horseMask.SetActive(false);
+    }
+
+    public void ChooseFox()
+    {
+        glasses.SetActive(false);
+        foxMask.SetActive(true);
+        horseMask.SetActive(false);
+    }
+
+    public void ChooseHorse()
+    {
+        glasses.SetActive(false);
+        foxMask.SetActive(false);
+        horseMask.SetActive(true);
+    }
+
+
+}
---
+++ Snap_AR/Assets/Scripts/SnapPic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SnapPic : MonoBehaviour {
+    public int num = 1;
+    public int picNum = 0;
+
+   public void TakePicAndDownload()
+    {
+        picNum += num;
+        print(picNum);
+        ScreenCapture.CaptureScreenshot(Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments),"screenshot" + picNum.ToString() + ".png"));
+    }
+
+
+
+   
+}
